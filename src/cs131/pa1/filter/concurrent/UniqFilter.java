40182c5393ea8161b5c947fc675c99e49a487487
Alex Hu <alexhubnds@icloud.com>
2017-09-10 00:51:45 -0400

    uniq filter

---
+++ src/cs131/pa1/filter/concurrent/UniqFilter.java
@@ -0,0 +1,11 @@
+package cs131.pa1.filter.concurrent;
+
+public class UniqFilter extends ConcurrentFilter{
+
+	@Override
+	public String processLine(String line) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
